ebe660caba449170278b605b647be6194c86b6ff
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-24 19:51:23 -0500

    Encapsulamiento primer funcion

--- scrapper_url.rb
+++ scrapper_url.rb
@@ -1,6 +1,26 @@
 require 'open-uri'
 require 'nokogiri'
 
+def traer_items(url)
+  items = []
+  doc_html = Nokogiri::HTML(URI.open(url).read)
+  pt = doc_html.search('.pagination').first.attributes['data-tp'].value.to_i
+  i = 1
+  pt.times do
+    page = "#{url}/pt-#{i}"
+    i += 1
+    Nokogiri::HTML(URI.open(page).read).search('.listado-viviendas .vivienda-item .title a').each do |title|
+      uri = URI.parse(title.attributes['href'].value)
+      uri.scheme = 'https'
+      uri.host = 'www.laencontre.com.pe'
+      uri.query = nil
+      # se almacena item en array
+      items << uri.to_s
+    end
+  end
+  items
+end
+
 def scrapper_properties(link)
   # Array que almacenará url's de las propiedades
   items_url = []
@@ -10,44 +30,14 @@ def scrapper_properties(link)
   url_ventas = "#{link}/venta/propiedades"
 
   # Scrapping para traer urls de items
-
-  # Traer el número de paginas de items
-  doc_html = Nokogiri::HTML(URI.open(url_ventas).read)
-  pt = doc_html.search('.pagination').first.attributes['data-tp'].value.to_i
-  i = 1
-  pt.times do
-    page = "#{url_ventas}/pt-#{i}"
-    i += 1
-    Nokogiri::HTML(URI.open(page).read).search('.listado-viviendas .vivienda-item .title a').each do |title|
-      url = URI.parse(title.attributes['href'].value)
-      url.scheme = 'https'
-      url.host = 'www.laencontre.com.pe'
-      url.query = nil
-      # se almacena item en array
-      items_url << url.to_s
-    end
-  end
+  items_url += traer_items(url_ventas)
 
   # Traer las urls de las propiedades en alquiler
   # Url propiedades en alquiler
   url_alquiler = "#{link}/alquiler/propiedades"
 
   # Scrapping para traer urls de items
-  doc_html = Nokogiri::HTML(URI.open(url_alquiler).read)
-  pt = doc_html.search('.pagination').first.attributes['data-tp'].value.to_i
-  i = 1
-  pt.times do
-    page = "#{url_alquiler}/pt-#{i}"
-    i += 1
-    Nokogiri::HTML(URI.open(page).read).search('.listado-viviendas .vivienda-item .title a').each do |title|
-      url = URI.parse(title.attributes['href'].value)
-      url.scheme = 'https'
-      url.host = 'www.laencontre.com.pe'
-      url.query = nil
-      # se almacena item en array
-      items_url << url.to_s
-    end
-  end
+  items_url += traer_items(url_alquiler)
 
   items_url.uniq
 end
