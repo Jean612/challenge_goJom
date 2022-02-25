83ef39d8d4ea33ef35e77bff0373063fdaf87f47
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-25 18:13:30 -0500

    Primera versión terminada

--- scrapper_property.rb
+++ scrapper_property.rb
@@ -1,13 +1,20 @@
 require_relative 'scrapper_url'
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 def scrapping_items
+  # llamo al metodo que me trae los links de los items con la url
   links = scrapper_properties('https://www.laencontre.com.pe/agente/palo-alto-inmobiliaria-37195')
+
+  # Array que almacenara los datos de cada item o propiedad
   data = []
   links.each do |link|
     html = Nokogiri::HTML(URI.open(link).read)
     id = "LE-#{link[-6..-1]}"
+
+    # Extracción de datos
+
     title = html.search('.detail-subtitle').first.text
     original_url = link
     original_pictures = html.search('.mfp-gallery picture img').map { |img| img.attributes['src'].value }
@@ -15,17 +22,37 @@ def scrapping_items
     header = html.search('h1').first.text.split(' ')
     property_type = {
       name: header[0],
-      slug: header[0].downcase
+      slug: header[0].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     }
     operation_type = {
       name: header[2],
-      slug: header[2].downcase
+      slug: header[2].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     }
     usd_price = html.search('.price h2').first.text.split(' ')[1].gsub(',', '').to_f
-    total_area = html.search('.dimensions').first.text.split('m')[0].to_i
+    total_area = html.search('.dimensions').first ? html.search('.dimensions').first.text.split('m')[0].to_i : 0
     bedrooms = html.search('.bedrooms').first ? html.search('.bedrooms').first.text.split(' ')[0].to_i : 0
     bathrooms = html.search('.bathrooms').first ? html.search('.bathrooms').first.text.split(' ')[0].to_i : 0
     garages = html.search('.garages').first ? html.search('.garages').first.text.split(' ')[0].to_i : 0
+    ubi = html.search('#locationShown').first ? html.search('#locationShown').first.attributes['value'].value : ''
+    location = {
+      address: html.search('.location_info').first ? html.search('.location h2 span').first.text : '',
+      country: 'Perú',
+      region: ubi.split(',')[2].strip,
+      province: ubi.split(',')[1].strip,
+      district: ubi.split(',')[0].strip,
+      zone: '',
+      geo_point: {
+        lat: html.search('.see-map').first ? html.search('.see-map').first.attributes['data-x'].value.to_f : '',
+        lon: html.search('.see-map').first ? html.search('.see-map').first.attributes['data-y'].value.to_f : ''
+      },
+      country_slug: 'peru',
+      region_slug: ubi.split(',')[2].strip.downcase.gsub(' ', '-').gsub(/[^\w-]/, ''),
+      province_slug: ubi.split(',')[1].strip.downcase.gsub(' ', '-').gsub(/[^\w-]/, ''),
+      district_slug: ubi.split(',')[0].strip.downcase.gsub(' ', '-').gsub(/[^\w-]/, ''),
+      zone_slug: ''.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    }
+
+    # ingresar nuevo hash con la data extraida al array de datos
 
     data << {
       id: id,
@@ -41,10 +68,23 @@ def scrapping_items
       build_area: total_area,
       bedrooms: bedrooms,
       bathrooms: bathrooms,
-      garages: garages
+      garages: garages,
+      location: location
     }
   end
-  data
+
+  # Si el file json existe sobreescribe la data, si no crea uno nuevo
+
+  if File.exist?('data.json')
+    File.open('data.json', 'wb') do |file|
+      file.write({ data: data }.to_json)
+    end
+  else
+    File.open('data.json', 'w') do |file|
+      file.write({ data: data }.to_json)
+    end
+  end
+
 end
 
 puts scrapping_items
--- scrapper_url.rb
+++ scrapper_url.rb
@@ -3,19 +3,23 @@ require 'nokogiri'
 
 def traer_items(url)
   items = []
-  doc_html = Nokogiri::HTML(URI.open(url).read)
+  doc_html = Nokogiri::HTML(URI.open(url).read)                                 # trae los datos de la pagina web conf formato nokogiri
   # Traer número de paginas del catalogo
-  pt = doc_html.search('.pagination').first.attributes['data-tp'].value.to_i
+  pt = doc_html.search('.pagination').first.attributes['data-tp'].value.to_i # 8
   i = 1
+  # itera tantas veces como sea el número de paginas del catalogo
   pt.times do
-    page = "#{url}/pt-#{i}"
+    # Creo la url de cada pagina del catalogo
+    page = "#{url}/p_#{i}"
+    # aumento 1 al indicador para en la siguiente iteracion cree la url con la siguiente pagina
     i += 1
+    # Extraigo la url de cada item de la pagina y le doy el formato de URI
     Nokogiri::HTML(URI.open(page).read).search('.listado-viviendas .vivienda-item .title a').each do |title|
       uri = URI.parse(title.attributes['href'].value)
       uri.scheme = 'https'
       uri.host = 'www.laencontre.com.pe'
       uri.query = nil
-      # se almacena item en array
+      # se almacena item en array con formato de url y en tipo string
       items << uri.to_s
     end
   end
